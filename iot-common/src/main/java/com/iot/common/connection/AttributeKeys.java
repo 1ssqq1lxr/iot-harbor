a2b2aa34a1ef2aa64393fb023565097e6739c747
陆旭荣 <luxurong@derhino.com>
2019-07-12 10:08:06 +0800

    ignore git

--- iot-common/src/main/java/com/iot/common/connection/AttributeKeys.java
+++ iot-common/src/main/java/com/iot/common/connection/AttributeKeys.java
@@ -2,6 +2,7 @@ package com.iot.common.connection;
 
 import io.netty.util.AttributeKey;
 import lombok.experimental.UtilityClass;
+import reactor.core.Disposable;
 
 @UtilityClass
 public class AttributeKeys {
@@ -9,6 +10,8 @@ public class AttributeKeys {
     public AttributeKey<ClientConnection> clientConnectionAttributeKey = AttributeKey.newInstance("client_operation");
 
     public AttributeKey<ServerConnection> serverConnectionAttributeKey = AttributeKey.newInstance("server_operation");
+
+    public AttributeKey<Disposable> closeConnection = AttributeKey.newInstance("close_connection");
 
 
 }
