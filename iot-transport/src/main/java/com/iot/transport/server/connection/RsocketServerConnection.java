036db4106a48a6fd8eb8a20c2b633a887967e468
陆旭荣 <luxurong@derhino.com>
2019-11-04 15:29:34 +0800

    reconnect

--- iot-transport/src/main/java/com/iot/transport/server/connection/RsocketServerConnection.java
+++ iot-transport/src/main/java/com/iot/transport/server/connection/RsocketServerConnection.java
@@ -84,8 +84,9 @@ public class RsocketServerConnection implements RsocketServerSession {
                             })));
             channelManager.removeConnections(connection); // 删除链接
             connection.getTopics().forEach(topic -> topicManager.deleteTopicConnection(topic, connection)); // 删除topic订阅
-            String  deviceId=connection.getConnection().channel().attr(AttributeKeys.device_id).get(); // 设置device Id
-            channelManager.removeDeviceId(deviceId);
+            Optional.ofNullable(connection.getConnection().channel().attr(AttributeKeys.device_id))
+                    .map(Attribute::get)
+                    .ifPresent(channelManager::removeDeviceId); // 设置device Id
         });
         inbound.receiveObject().cast(MqttMessage.class)
                 .subscribe(message -> messageRouter.handler(message, connection));
